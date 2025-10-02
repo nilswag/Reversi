a2d21cecc1521f8ec8af91653656c91cab3a9f59
nilswag <nilswagemaker05@icloud.com>
2025-10-02 15:26:02 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Reversi
+{
+
+    internal static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+}
