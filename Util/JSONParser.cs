6a7c3d4c02f71674acca594f9af7f6789208c81e
nilswag <nilswagemaker05@icloud.com>
2025-10-03 15:53:20 +0200

    Started working on json parser

--- Util/JSONParser.cs
+++ Util/JSONParser.cs
@@ -1,7 +1,43 @@
 ﻿
+using System.Text.Json;
+
 namespace Reversi.Util
 {
+
+    /// <summary></summary>
+    /// Based on: https://www.pietschsoft.com/post/2024/06/18/csharp-read-text-and-json-file-contents-into-variable-in-memory
     public class JSONParser
     {
+        /// <summary>String that contains the file path to the json file.</summary>
+        public string FilePath { get; }
+
+        private readonly JsonElement? Root;
+
+        public JSONParser(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public bool Load()
+        {
+            try
+            {
+                using FileStream fs = new(FilePath, FileMode.Open, FileAccess.Read);
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Unable to load JSON: {e.Message}");
+                return false;
+            }
+
+            return true;
+        }
+
+        public T GetProperty<T>(string key)
+        {
+
+            return null;
+        }
+
     }
+
 }
