d93de363ecb09da07cc2dd315da819d4922b7c85
nilswag <nilswagemaker05@icloud.com>
2025-10-12 19:30:01 +0200

    Almost done with algorithm

--- Game.cs
+++ Game.cs
@@ -28,36 +28,65 @@ namespace Reversi
             _mainForm.Refresh();
         }
 
-        public bool IsValidMove(Piece player, int r, int c)
+        public bool IsValidMove(Piece player, Piece opponent, int r, int c, List<(int, int)> flips)
         {
             // If the current spot is not empty, just return since there is no need to check if its valid.
             if (_board.Grid[r, c] != Piece.EMPTY) return false;
 
             // Specify dr (=delta rows) and dc (=delta columns) in an array.
-            (int, int)[] directions = new (int, int)[]
+            (int, int)[] directions =
             {
                 (-1, -1),   (0, -1),    (1, -1),
                 (-1,  0),               (1,  0),
                 (-1,  1),   (0,  1),    (1,  1)
             };
 
+            List<(int, int)> tempFlips = []; 
             // Loop through each dr and dc
             foreach((int dr, int dc) in directions)
             {
                 // nr = new row, nc = new column, apply the delta to the new values.
                 int nr = r + dr, nc = c + dc;
                 // Check for out of bounds and if the new spot is not empty (then the diagonal would not be outflanked).
-                while (nr > 0 && nc > 0 && nr < _board.NCells && nc < _board.NCells && _board.Grid[nr, nc] != Piece.EMPTY)
+                while (nr >= 0 && nc >= 0 && nr < _board.NCells && nc < _board.NCells && _board.Grid[nr, nc] != Piece.EMPTY)
                 {
-                    
+                    // If there are any opponent pieces in the way add them to-be-flipped list.
+                    if (_board.Grid[nr, nc] == opponent) tempFlips.Add((nr, nc));
+                    // If there is at least one opponent piece in the to-be-flipped list and a player piece is found (thus the opponent pieces are flanked),
+                    // then the move is valid.
+                    if (_board.Grid[nr, nc] == player && tempFlips.Count > 0)
+                    {
+                        flips.AddRange(tempFlips);
+                        return true;
+                    }
+                    nr += dr;
+                    nc += dc;
                 }
             }
 
-            return true;
+            return false;
         }
 
-        public void GetMoves(Piece player = Piece.PLAYER1)
-        { }
+        public List<((int, int), List<(int, int)>)> GetMoves(Piece player)
+        {
+            // Basic logic for finding the opponent.
+            Piece opponent = player != Piece.PLAYER1 ? Piece.PLAYER1 : Piece.PLAYER2; 
+            List<((int, int), List<(int, int)>)> moves = [];
+
+            // Loop through each position in the board.
+            for (int r = 0; r < _board.NCells; r++)
+            {
+                for (int c = 0; c < _board.NCells; c++)
+                {
+                    List<(int, int)> flips = [];
+                    // If the move would be valid add them to the possible moves.
+                    if (IsValidMove(player, opponent, r, c, flips))
+                        moves.Add(((r, c), flips));
+                }
+            }
+
+            return moves;
+        }
 
     }
 }
--- Main.cs
+++ Main.cs
@@ -14,7 +14,17 @@ namespace Reversi
 
             Board board = new Board(ClientSize, 500, 4);
             Game game = new Game(this, board);
-            game.GetMoves();
+
+            string str = "";
+            foreach (((int r, int c), var flips) in game.GetMoves(Piece.PLAYER1))
+            {
+                str = $"[{r}, {c}] = [";
+                foreach ((int rf, int cf) in flips)
+                    str += $"({rf}, {cf}), ";
+                str = str.Remove(str.Length - 2);
+                str += "]\n";
+            }
+            Console.WriteLine(str);
 
             Controls.Add(board);
         }
